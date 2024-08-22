654fd910cf9b6fe924874176deac303215a507d4
drifter089 <akshatmittal.0007@gmail.com>
2024-08-22 17:57:28 +0700

    updated set deposition function

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ zeno = zenodopy.Client(sandbox=True)
 
 # zeno.list_projects
 
-zeno.create_project(
-    title="twoversionci",
-    upload_type="other",
-    metadata_json=".zenodo.json",
-)
+# zeno.create_project(
+#     title="twoversionci",
+#     upload_type="other",
+#     metadata_json=".zenodo.json",
+# )
+zeno.set_project(dep_id=102296)
 
 
 # upload file to zenodo
-zeno.upload_file("test.file.txt", publish=True)
+# zeno.upload_file("test.file.txt", publish=True)
 zeno.update(
     source="/home/akshat/zenodopy",
     publish=True,
